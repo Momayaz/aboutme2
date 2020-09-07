5abcf66a50d235ce50348ac0ddd7d8c084320a1a
momayaz <momayaz.dr.1@gmail.com>
2020-09-07 21:04:51 +0300

    about me

---
+++ js/app.js
@@ -0,0 +1,42 @@
+'use strict'
+var q1= prompt('is my major Translation?^.^').toLowerCase();
+   
+if(q1=='yes'||q1=='y'){
+//console.log('correct answer')
+alert('correct answer')
+}else if(q1=='no'||q1=='n'){
+//console.log('incorrect answer')
+alert('incorrect answer')
+} else {                              
+alert('bad input')
+}
+var q2= prompt('Do you think i graduated in low avarage?').toLowerCase()
+if(q2=='yes'||q2=='y'){
+ //onsole.log('ammazing,correct again')
+ alert('ammazing,correct again')
+}else if(q2=='no'||q2=='n'){
+// console.log('oops,incorrect')
+alert('oops,incorrect')
+}else {
+alert('bad input')
+}
+var q3= prompt('Do you think am hardworker?').toLowerCase()
+if(q3=='yes'||q3=='y'){
+    //console.log('correct answer ^^')
+    alert('correct answer ^^')
+}else if(q3=='no'||q3=='n'){
+//console.log('oppsi,incorrect answer')
+    alert('oppsi,incorrect answer')
+}else{
+    alert('bad input')
+}
+var q4=prompt('do you think i will stop there?').toLowerCase()
+if(q4=='yes'||q4=='y'){
+//console.log('thank you alot for you suppot^^')
+    alert('thank you alot for your support')
+}else if(q4=='no'||q4=='n'){
+//console.log('thank you, i will do my best ^^')
+    alert('thank you, i will do my beast')
+}else{
+    alert('bad input')
+}
